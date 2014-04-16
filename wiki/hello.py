40aa7a3d5f936b6c07afa9b356422cd63b5ecb2c
Dmitriy Verbitskiy <verver94@gmail.com>
2014-04-16 04:50:56 +0400

    so close to prototype

--- wiki/hello.py
+++ wiki/hello.py
@@ -9,11 +9,12 @@ from lxml import html, etree
 
 def hello(request):
 	
-	#url = "http://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F"
+	url = "http://ru.wikipedia.org/wiki/%D0%A0%D0%BE%D1%81%D1%81%D0%B8%D1%8F"
 	#page = html.fromstring(urllib.urlopen(url).read())
 
 	#cont = page.xpath("//div[@id='content']")
 	t = get_template('index.html')
 	#string = html.tostring(cont)
-	htmlq = t.render(Context())
+	increment = 0
+	htmlq = t.render(Context({'startPosition': url, 'inc' : increment}))
 	return HttpResponse(htmlq)	
